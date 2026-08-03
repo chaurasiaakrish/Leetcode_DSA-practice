603837d6e5c78e00cc37b5b2e446e856d0dd83cf
Akrish Chaurasia <173398181+chaurasiaakrish@users.noreply.github.com>
2026-08-03 16:41:18 +0530

    Time: 377 ms (5.02%) | Memory: 19.9 MB (5.34%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -5,7 +5,7 @@ class Solution:
       res=0
       for j in range(len(s)):
         freq[s[j]]=freq.get(s[j],0)+1
-        if (len(freq)!=(j-i+1)):
+        while(len(freq)!=(j-i+1)):
             freq[s[i]]-=1
             if freq[s[i]]==0:
                 del freq[s[i]]
