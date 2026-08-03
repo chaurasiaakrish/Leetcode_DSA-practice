405ef7aecf7a39a89fec9f3b6f634cc5422983aa
Akrish Chaurasia <173398181+chaurasiaakrish@users.noreply.github.com>
2026-08-03 14:49:02 +0530

    Time: 205 ms (19.66%) | Memory: 25.9 MB (34.24%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        low=0
+        freq={}
+        res=-1
+        for high in range(len(fruits)):
+            freq[fruits[high]]=freq.get(fruits[high],0)+1
+            while(len(freq)>2):
+                freq[fruits[low]]-=1
+                if freq[fruits[low]]==0:
+                    del freq[fruits[low]]  
+                low+=1  
+            if (len(freq)<=2):
+                length=(high-low)+1
+                res=max(res,length) 
+        return res        
