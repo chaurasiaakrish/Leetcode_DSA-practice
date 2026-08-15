7bf44e29f8e0581ac245e6931c490058ed969a72
Akrish Chaurasia <173398181+chaurasiaakrish@users.noreply.github.com>
2026-08-16 00:06:15 +0530

    Time: 17 ms (11.10%) | Memory: 21 MB (50.20%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,8 +1,32 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        l=[]
-        for i in range(len(nums)):
-            l.append(nums[i]*nums[i])
-        return sorted(l)    
+        # l=[]
+        # for i in range(len(nums)): # O(n)
+        #     l.append(nums[i]*nums[i])
+        # return sorted(l)    # O(nlogn)
 
-        
+        res=[]
+        pos_list=[]
+        neg_list=[]
+        i=0
+        j=0
+        for k in range(len(nums)):
+            if nums[k]<0:
+                neg_list.append(nums[k]*nums[k])
+            else:
+                pos_list.append(nums[k]*nums[k])
+        neg_list=neg_list[::-1]          
+        while(i<len(pos_list) and j<len(neg_list)):
+            if pos_list[i]>neg_list[j]:
+                res.append(neg_list[j])
+                j+=1
+            else:
+                res.append(pos_list[i])
+                i+=1         
+        while(j<len(neg_list)):
+            res.append(neg_list[j])
+            j+=1
+        while(i<len(pos_list)):
+            res.append(pos_list[i]) 
+            i+=1 
+        return res
