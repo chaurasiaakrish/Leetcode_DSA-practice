bfe8d7261e6460822ce356a175a3f3047e1e6ae3
Akrish Chaurasia <173398181+chaurasiaakrish@users.noreply.github.com>
2026-08-18 12:25:44 +0530

    Time: 7 ms (37.01%) | Memory: 19.7 MB (78.73%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        prod=nums[0]
+        max_prod=nums[0]
+        min_prod=nums[0]
+        res=nums[0]
+        for i in range(1,len(nums)):
+            prod=nums[i]                # to only consoder that number
+            max_prod=max_prod*nums[i]   # for tracking max_prod
+            min_prod=min_prod*nums[i]   # for tracking min_prod
+            maxx=max(prod,max(max_prod,min_prod))
+            minn=min(prod,min(max_prod,min_prod))
+            max_prod=maxx
+            min_prod=minn
+            res=max(res,max(maxx,minn))
+        return res  
